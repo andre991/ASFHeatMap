ccc63b86b211fe3adefa2708debe69182740c608
André  Santana Ferreira <andre.991@hotmail.com>
2017-03-15 16:48:30 -0300

    Initial Commit

---
+++ ASFHeatMap/ASFHeatMap.h
@@ -0,0 +1,19 @@
+//
+//  ASFHeatMap.h
+//  ASFHeatMap
+//
+//  Created by Andre Santana Ferreira on 15/03/17.
+//  Copyright © 2017 Andre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ASFHeatMap.
+FOUNDATION_EXPORT double ASFHeatMapVersionNumber;
+
+//! Project version string for ASFHeatMap.
+FOUNDATION_EXPORT const unsigned char ASFHeatMapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ASFHeatMap/PublicHeader.h>
+
+
